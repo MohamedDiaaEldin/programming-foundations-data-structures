661af6d19b8a1802874a2fbf7cc5f628134a97f5
mohamed <mdiaa404@gmail.com>
2021-09-17 21:07:21 +0200

    hash table

---
+++ my-files/hash-table/hash_table.py
@@ -0,0 +1,11 @@
+
+# create new hash table  
+student_grades = {"mohamed":50, "":60, "hossam":22}
+
+student_grades['zakria'] = 100 
+student_grades['ali'] = 70  # update ali value 
+
+
+print(student_grades)                # print all hash table
+print(type(student_grades))          # print type of this object 
+print(student_grades['mohamed'])     # print value of key mohamed
