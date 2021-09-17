0626921022d2bd8fc43be46cc56fc1af459b4023
mohamed <mdiaa404@gmail.com>
2021-09-17 21:33:53 +0200

    sets

---
+++ my-files/hash_table-set/sets.py
@@ -0,0 +1,9 @@
+student_names  =  ['mohamed', 'mohamed', 'ali', "Ali", 'ali', 'Hossam']
+
+uniqe_students = set(student_names)
+
+print(uniqe_students)               # print all set 
+
+print('mohamed' in student_names)   # checks if mohamed is in the set 
+print('zakria' in student_names)    # checks if zakrio is in the set 
+print(type(uniqe_students))         # print the type of set data structure 
